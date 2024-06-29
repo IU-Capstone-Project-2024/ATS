cce5701454e4f41146f55453b1ac3281385c7968
Kashapov Shamil <favelanky@gmail.com>
2024-06-29 20:19:19 +0300

    update ui for windows

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,25 +1,23 @@
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import WordCompleter
-from prompt_toolkit.shortcuts import input_dialog
+import os
+from dotenv import load_dotenv
+import questionary
 
 
 class UserInterface:
     def _select_from_list(self, prompt_text, options):
-        completer = WordCompleter(options, ignore_case=True)
-        return prompt(f"{prompt_text}: ", completer=completer)
+        return questionary.select(prompt_text, choices=options).ask()
 
     def _get_user_input(self, prompt, default=None):
-        response = input(f"{prompt} [{default}]: ")
-        return response if response else default
+        return questionary.text(f"{prompt} [{default}]:").ask() or default
 
     def create_order(self):
         category = "spot"  # Default value
         symbol = "BTCUSDT"  # Default value
         side = self._select_from_list("Select side", ["Buy", "Sell"])
-        order_type = "Limit"
+        order_type = "Limit"  # Default value
         qty = self._get_user_input("Enter quantity", "0.001")
         price = self._get_user_input("Enter price", "1000")
-        time_in_force = "GTC"
+        time_in_force = "GTC"  # Default value
 
         order_params = {
             "category": category,
