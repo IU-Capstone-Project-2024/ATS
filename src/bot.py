361d90062cf579888da14ad81214021f5d5ee41d
Kashapov Shamil <favelanky@gmail.com>
2024-06-29 20:44:13 +0300

    add actions

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,8 @@ from user_interface import UserInterface
 from logger import Logger
 from bybit import BybitAPI
 
+import questionary
+
 
 class TradingBot:
     def __init__(self, mode):
@@ -15,23 +17,36 @@ class TradingBot:
 
     def run(self):
         while True:
-            order_params = None
+            action = self.select_action()
 
+            self.do_action(action)
+
+            time.sleep(1)
+
+    def select_action(self):
+        actions = ["create_order", "get_unfilled_orders", "cancel_order", "get_historical_data"]
+
+        return questionary.select("Select action", choices=actions).ask()
+
+    def do_action(self, action):
+        if action == "create_order":
+            order_params = None
             if self.mode == 'ml':
                 order_params = self.ml_interface.get_price_signal()
             elif self.mode == 'user':
                 order_params = self.user_interface.create_order()
 
-            self.execute_trade(order_params)
-            time.sleep(1)
-
-    def execute_trade(self, order_params):
-        response = self.bybit.create_order(**order_params)
-
-        print("Create Order Response:", response)
+            return self.execute_trade(order_params)
+        elif action == "get_unfilled_orders":
+            return self.bybit.get_unfilled_orders()
+        elif action == "cancel_order":
+            order_id = input("Enter order ID: ")
+            return self.bybit.cancel_order(order_id)
+        elif action == "get_historical_data":
+            return self.bybit.get_historical_data()
 
 
 if __name__ == '__main__':
-    mode = input("Enter mode (ml/user): ")
+    mode = questionary.select("Select mode", choices=["ml", "user"]).ask()
     bot = TradingBot(mode)
     bot.run()
